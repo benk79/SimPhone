c30ca1288d287edc26bc68928c0f8d54b3c4f995
bastienpitton <bastien732@gmail.com>
2019-05-25 09:50:05 +0200

    ça fonctionne !!!

--- src/calendar/QuelJour.java
+++ src/calendar/QuelJour.java
@@ -1,164 +1,101 @@
 package calendar;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class QuelJour {
 
 	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd ");
 
-		Calendar cal = new GregorianCalendar();
+		Calendar calendar = new GregorianCalendar();
 
-		int annee = cal.get(Calendar.YEAR);
-		int mois = cal.get(Calendar.MONTH);
+		int cannee = calendar.get(Calendar.YEAR);
+		int cmois = calendar.get(Calendar.MONTH);
 
-		int jourdelasemaine = cal.get(Calendar.DAY_OF_WEEK);
-		jourdelasemaine = jourdelasemaine - 1;
-		String jourecrit = null;
-		String moisecrit = null;
+	//	System.out.println(cmois);
+		//System.out.println(cannee);
 
-		switch (jourdelasemaine) {
+		// calendar = new GregorianCalendar(1913,10,23);
 
-		case 1: {
-			jourecrit = "lundi";
+		calendar = new GregorianCalendar(cannee, cmois, 0);
+
+	//	System.out.println(sdf.format(calendar.getTime()));
+
+		int cjourdelasemaine = calendar.get(Calendar.DAY_OF_WEEK);
+		int csemaine[] = new int[7];
+	//	System.out.println(cjourdelasemaine);
+
+		for (int i = 0; i < csemaine.length; i++) {
+
+			if (cjourdelasemaine > 7) {
+				cjourdelasemaine = cjourdelasemaine - 7;
+			}
+
+			csemaine[i] = cjourdelasemaine;
+			cjourdelasemaine = cjourdelasemaine + 1;
+		}
+
+		for (int i = 0; i < csemaine.length; i++) {
+
+			System.out.println(csemaine[i]);
+
+		}
+
+		char csemainelettre[]= new char[7];
+		for (int i = 0; i < csemaine.length; i++) {
+
+			csemainelettre[i]=choisirlettre(csemaine[i]);
+
+		}
+		
+		for (int i = 0; i < csemaine.length; i++) {
+
+			System.out.println(csemainelettre[i]);
+
+		}
+		
+			//int nimportequoi=4;	
+			//char lettretest=choisirlettre(nimportequoi);
+			//System.out.println(lettretest);
+		}
+
+		
+	
+
+	public static char choisirlettre(int numero) {
+		numero=numero-1;
+		char lettre = 0;
+
+		switch (numero) {
+
+		case 0:
+			lettre = 'L';
 			break;
-		}
-
-		case 2: {
-			jourecrit = "mardi";
-			break;
-		}
-
-		case 3: {
-			jourecrit = "mecredi";
-			break;
-		}
-
-		case 4: {
-			jourecrit = "jeudi";
-			break;
-		}
-
-		case 5: {
-			jourecrit = "vendredi";
-			break;
-		}
-
-		case 6: {
-			jourecrit = "samedi";
-			break;
-		}
-
-		case 7: {
-			jourecrit = "dimanche";
-			break;
-		}
-
-		}
-
-		switch (mois) {
-		case 1: {
-			moisecrit = "janvier";
-			break;
-		}
-		case 2: {
-			moisecrit = "fevrier";
-			break;
-		}
-
-		case 3: {
-			moisecrit = "mars";
-			break;
-		}
-
-		case 4: {
-			moisecrit = "avril";
-			break;
-		}
-
-		case 5: {
-			moisecrit = "mai";
-			break;
-		}
-
-		case 6: {
-			moisecrit = "juin";
-			break;
-		}
-
-		case 7: {
-			moisecrit = "juillet";
-			break;
-		}
-
-		case 8: {
-			moisecrit = "aout";
-			break;
-		}
-
-		case 9: {
-			moisecrit = "septembre";
-			break;
-		}
-
-		case 10: {
-			moisecrit = "octobre";
-			break;
-		}
-
-		case 11: {
-			moisecrit = "novembre";
-			break;
-		}
-
-		case 12: {
-			moisecrit = "decembre";
-		}
-			break;
-		}
-
-		int nombrejoursmois;
-
-		boolean bisex = false;
-
-		if (annee % 4 == 0 && annee % 100 != 0) {
-			bisex = true;
-		}
-
-		if (annee % 400 == 0) {
-			bisex = true;
-		}
-
-		mois = mois + 1;
-
-		switch (mois) {
-
 		case 1:
+			lettre = 'M';
+			break;
+		case 2:
+			lettre = 'M';
+			break;
 		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-
-			nombrejoursmois = 31;
+			lettre = 'J';
 			break;
-
 		case 4:
-		case 6:
-		case 9:
-		case 11:
-
-			nombrejoursmois = 30;
+			lettre = 'V';
 			break;
+		case 5:
+			lettre = 'S';
+			break;
+		case 6:
+			lettre = 'D';
+			break;
+
 		}
 
-		if (mois == 2 && bisex == true) {
-			nombrejoursmois = 29;
-		} else {
-			nombrejoursmois = 28;
-		}
-
+		return lettre;
 	}
 
 }
--- src/calendar/calendarApp.java
+++ src/calendar/calendarApp.java
@@ -1,12 +1,18 @@
 package calendar;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentListener;
+import java.awt.font.TextAttribute;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.zip.Inflater;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -14,21 +20,151 @@ import javax.swing.JPanel;
 
 import main.Application;
 
-public class calendarApp extends Application  {
+public class calendarApp extends Application implements ActionListener {
 
+	protected int moiscourrant;
+	private JButton avant = new JButton("precedant");
+	private JButton apres = new JButton("suivant");
+
+	//	private JPanel   m superieur1;
+	
+	
 	public calendarApp() {
 		super("Calendrier");
+
+		// donnée sur les chiffres
+
+		Calendar cal = new GregorianCalendar();
+		int annee = cal.get(Calendar.YEAR);
+		int moiscourant = cal.get(Calendar.MONTH);
+
+		int moisavant = cal.get(Calendar.MONTH);
+		moisavant = moisavant - 1;
+
+		int moisapres = cal.get(Calendar.MONTH);
+		moisapres = moisapres + 1;
+
+		int jourdelasemaine = cal.get(Calendar.DAY_OF_WEEK);
+		int datedujour = cal.get(Calendar.DAY_OF_MONTH);
+		int nombrejoursmois;
+		String moisecrit = null;
+		boolean bisex = false;
+		bisex = bisextile();
+		nombrejoursmois = direnombrejour(bisex, moiscourant);
+		direlejourdelasemaine(jourdelasemaine);
+		moisecrit = direlenomdumois(moiscourant);
+
+		// jpanel superieur
+
+		screen.setLayout(new BorderLayout());
+
+		// on annonce les elements
+
+		String moisavantecrit = direlenomdumois(moisavant);
+		String moisapresecrit = direlenomdumois(moisapres);
+
+		JLabel JLabelmoiscourant = new JLabel(moisecrit);
+		JLabel JLabelanneecourante = new JLabel(String.valueOf(annee));
+
+		JLabelmoiscourant.setForeground(Color.blue);
+		JLabelanneecourante.setForeground(Color.blue);
+		JPanel superieur1 = new JPanel();
+
+		// on les ajoute
+
+		superieur1.setLayout(new FlowLayout());
+
+		superieur1.add(avant);
+		superieur1.add(JLabelmoiscourant);
+		superieur1.add(JLabelanneecourante);
+
+		superieur1.add(apres);
+
+		apres.addActionListener(this);
+
+		avant.addActionListener(this);
+
+		char tablettre[] = new char[7];
+		int chiffretab[] = new int[7];
+
+		for (int i = 0; i < 7; i++) {
+
+			chiffretab[i] = tabpourlettre(i);
+
+		}
+
+		for (int i = 0; i < 7; i++) {
+
+			tablettre[i] = choisirlettre(chiffretab[i]);
+
+		}
+
+		
+		//jpanel intermediaire
+		// creer tab jsemaine
+		// JLabel js1 = new JLabel(" puisse dieu nous venir en aide");
+		JLabel[] jsemaine = new JLabel[7];
+
+		for (int i = 0; i < 7; i++) {
+			jsemaine[i] = new JLabel(String.valueOf(tablettre[i]));
+
+			jsemaine[i].setForeground(Color.MAGENTA);
+		}
+
+		// creer le jpanel
+		JPanel superieur2 = new JPanel();
+		superieur2.setLayout(new GridLayout());
+
+		//on ajoute jsemaine
+		//	superieur2.add(js1);
+
+		for (int i = 0; i < 7; i++) {
+
+		// (Jsemaine[i] == d) {
+		// jsemaine[i].setForeground(Color.green);
+		// }
+
+			superieur2.add(jsemaine[i]);
+
+		}
+
+		// jpanel du bas 
+		// creer tab labeljour
+		JLabel[] labelJours = new JLabel[nombrejoursmois];
+		for (int i = 0; i < nombrejoursmois; i++) {
+			labelJours[i] = new JLabel((Integer.toString(i + 1)));
+		}
+		// creer le panel
+		JPanel central = new JPanel();
+		central.setLayout(new GridLayout(6, 7, 5, 10));
+
+		// on ajoute le tab dans le panel
+		for (int i = 0; i < nombrejoursmois; i++) {
+			if (i == datedujour) {
+				labelJours[i].setForeground(Color.green);
+			}
+			central.add(labelJours[i]);
+		}
+
+		// gerer espace vide
+		int gridEmptyPlaces = 6 * 7 - nombrejoursmois;
+		for (int i = 0; i < gridEmptyPlaces; i++) {
+			central.add(new JLabel(""));
+		}
+
+		// mettre ensemble les jpanel
+
+		screen.add(superieur1, BorderLayout.NORTH);
+		screen.add(superieur2, BorderLayout.CENTER);
+		screen.add(central, BorderLayout.SOUTH);
+
+	}
+
+	public static boolean bisextile() {
 
 		Calendar cal = new GregorianCalendar();
 
 		int annee = cal.get(Calendar.YEAR);
-		int moiscourant = cal.get(Calendar.MONTH);
-
-		int nombrejoursmois;
-		String moisecrit=null;
-		
-		System.out.println(annee);
-
 		boolean bisex = false;
 
 		if (annee % 4 == 0 && annee % 100 != 0) {
@@ -39,8 +175,14 @@ public class calendarApp extends Application  {
 			bisex = true;
 		}
 
+		return bisex;
+	}
+
+	public static int direnombrejour(boolean bisex, int moiscourant) {
+
 		moiscourant = moiscourant + 1;
 
+		int nombrejoursmois;
 		switch (moiscourant) {
 
 		case 4:
@@ -56,194 +198,113 @@ public class calendarApp extends Application  {
 			} else {
 				nombrejoursmois = 28;
 			}
-		default :
+		default:
 			nombrejoursmois = 31;
-		}
-
-		
-
-		int jourdelasemaine = cal.get(Calendar.DAY_OF_WEEK);
-		jourdelasemaine = jourdelasemaine - 1;
-		String jourecrit = null;
-
-		switch (jourdelasemaine) {
-
-		case 1: {
-			jourecrit = "lundi";
-			break;
-		}
-
-		case 2: {
-			jourecrit = "mardi";
-			break;
-		}
-
-		case 3: {
-			jourecrit = "mecredi";
-			break;
-		}
-
-		case 4: {
-			jourecrit = "jeudi";
-			break;
-		}
-
-		case 5: {
-			jourecrit = "vendredi";
-			break;
-		}
-
-		case 6: {
-			jourecrit = "samedi";
-			break;
-		}
-
-		case 7: {
-			jourecrit = "dimanche";
-			break;
-		}
 
 		}
-
-		switch (moiscourant) {
-		case 1: {
-			moisecrit = "janvier";
-			break;
-		}
-		case 2: {
-			moisecrit = "fevrier";
-			break;
-		}
-
-		case 3: {
-			moisecrit = "mars";
-			break;
-		}
-
-		case 4: {
-			moisecrit = "avril";
-			break;
-		}
-
-		case 5: {
-			moisecrit = "mai";
-			break;
-		}
-
-		case 6: {
-			moisecrit = "juin";
-			break;
-		}
-
-		case 7: {
-			moisecrit = "juillet";
-			break;
-		}
-
-		case 8: {
-			moisecrit = "aout";
-			break;
-		}
-
-		case 9: {
-			moisecrit = "septembre";
-			break;
-		}
-
-		case 10: {
-			moisecrit = "octobre";
-			break;
-		}
-
-		case 11: {
-			moisecrit = "novembre";
-			break;
-		}
-
-		case 12: {
-			moisecrit = "decembre";
-		}
-			break;
-		}
-		
-		
-		//
-		JLabel titleLabel = new JLabel("<html><h1>" + getName() + "</h1></html>");
-
-		screen.add(titleLabel);
-		screen.setLayout(new BorderLayout());
-
-		JButton arriere = new JButton("ARRIERE");
-		JLabel JLabelmoiscourant = new JLabel(moisecrit);
-		JLabel JLabelanneecourante = new JLabel(String.valueOf(annee));
-		JButton avant = new JButton("AVANT");
-
-		
-		
-		 
-		//int nbJours = 31;
-
-		
-		JLabel[] labelJours = new JLabel[nombrejoursmois];
-
-		
-		for(int i=0;i<nombrejoursmois;i++) {
-		labelJours[i]= new JLabel((Integer.toString(i+1))); 	
-		}
-		
-		
-
-		JLabel[] addJours = new JLabel[31];
-		
-		
-		for (int i = 0; i < 31; i++) {
-
-			addJours[i] = new JLabel(Integer.toString(i));
-
-		}
-
-		JPanel superieur1 = new JPanel();
-		superieur1.setLayout(new BorderLayout());
-		superieur1.add(arriere, BorderLayout.WEST);
-		superieur1.add(avant, BorderLayout.EAST);
-		//superieur1.add(JLabelmoiscourant, BorderLayout.CENTER);
-		//superieur1.add(JLabelanneecourante, BorderLayout.SOUTH);
-		
-		JPanel superieur2= new JPanel();
-		superieur2.setLayout(new BorderLayout());
-		superieur2.add(JLabelmoiscourant, BorderLayout.WEST);
-		superieur2.add(JLabelanneecourante, BorderLayout.EAST);
-		
-		JPanel central = new JPanel();
-		central.setLayout(new GridLayout(6, 7, 5, 10));
-
-		
-		/*
-		 * Add empty labels to fill home screen grid layout
-		 */
-		
-		for(int i=0;i<nombrejoursmois;i++) {
-			
-			central.add(labelJours[i]);
-			
-		}
-		
-		
-		int gridEmptyPlaces = 6 * 7 - nombrejoursmois;
-
-		for (int i = 0; i < gridEmptyPlaces; i++) {
-			central.add(new JLabel(""));
-		}
-
-	//	superieur1.addComponentListener(this) ;
-		
-		
-		
-		
-		screen.add(superieur1, BorderLayout.NORTH);
-		screen.add(superieur2, BorderLayout.CENTER);
-		screen.add(central, BorderLayout.SOUTH);
+		return nombrejoursmois;
 
 	}
 
+	public static char[] direlejourdelasemaine(int jourdelasemaine) {
+
+		char[] semaine = new char[7];
+
+		for (int i = 0; i < semaine.length; i++) {
+
+		}
+
+		return semaine;
+	}
+
+	public static String direlenomdumois(int moiscourant) {
+
+		String moisecrit = null;
+		switch (moiscourant) {
+		case 1: return "janvier";
+		case 2: return "fevrier";
+		case 3: return "mars";
+		case 4: return "avril";
+		case 5: return "mai";
+		case 6: return "juin";
+		case 7: return "juillet";
+		case 8: return "aout";
+		case 9: return "septembre";
+		case 10:return  "octobre";
+		case 11:return "novembre";
+		case 12:return "decembre";
+		}
+		return moisecrit;
+
+	}
+
+	public static int augmentermois(int moiscourant) {
+		moiscourant = moiscourant;
+		return moiscourant;
+	}
+
+	public static char choisirlettre(int numero) {
+		char lettre = 0;
+		switch (numero) {
+		case 0: return 'L';
+		case 1: return'M';
+		case 2:	return 'M';
+		case 3:	return 'J';
+		case 4: return 'V';
+		case 5: return 'S';
+		case 6:return 'D';
+		}
+		return lettre;
+	}
+
+	public static int tabpourlettre(int numero) {
+
+		Calendar calendar = new GregorianCalendar();
+
+		int cannee = calendar.get(Calendar.YEAR);
+		int cmois = calendar.get(Calendar.MONTH);
+
+		// calendar = new GregorianCalendar(1913,10,23);
+
+		calendar = new GregorianCalendar(cannee, cmois, 0);
+
+		// System.out.println(sdf.format(calendar.getTime()));
+
+		int cjourdelasemaine = calendar.get(Calendar.DAY_OF_WEEK);
+		int[] csemaine = new int[7];
+
+		cjourdelasemaine = cjourdelasemaine - 1;
+		for (int i = 0; i < csemaine.length; i++) {
+
+			if (cjourdelasemaine > 6) {
+				cjourdelasemaine = cjourdelasemaine - 7;
+			}
+
+			csemaine[i] = cjourdelasemaine;
+			cjourdelasemaine = cjourdelasemaine + 1;
+		}
+
+		return csemaine[numero];
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		System.out.println("click avant");
+
+		if (e.getSource() == avant) {
+			moiscourrant = moiscourrant - 1;
+		}
+		if (e.getSource() == apres) {
+			moiscourrant = moiscourrant + 1;
+		}
+
+	}
+	public static void raffraichir(JPanel superieur1,JPanel superieur2, JPanel central) {
+		
+		screen.remove(superieur1);
+		screen.remove(superieur2);
+		screen.remove(central);
+	}
 }
