079b0966eb7227ea6503f79f4bc9e450a5a395a0
Bretz Louise <bretzlouise@gmail.com>
2019-06-12 22:38:01 +0200

    Javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"calendar"},{"l":"contact"},{"l":"gallery"},{"l":"helloworld"},{"l":"junitTest"},{"l":"main"},{"l":"tictactoe"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"calculator"},{"l":"calendar"},{"l":"contact"},{"l":"gallery"},{"l":"helloworld"},{"l":"junitTest"},{"l":"main"},{"l":"tictactoe"}]
